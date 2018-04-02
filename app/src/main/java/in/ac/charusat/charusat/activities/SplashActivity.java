01ceb1194dfc3480b189d5be3477180cc61f30a2
Darshan <DozzerCoder>
2018-04-02 13:44:35 +0530

    Splash Background changes

--- app/src/main/java/in/ac/charusat/charusat/activities/SplashActivity.java
+++ app/src/main/java/in/ac/charusat/charusat/activities/SplashActivity.java
@@ -18,14 +18,15 @@ public class SplashActivity extends AwesomeSplash {
 
 
         //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.white); //any color you want form colors.xml
-        configSplash.setAnimCircularRevealDuration(700); //int ms
-        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);  //or Flags.REVEAL_LEFT
-        configSplash.setRevealFlagY(Flags.REVEAL_TOP); //or Flags.REVEAL_TOP
+        configSplash.setBackgroundColor(R.color.splash_background); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(900); //int ms
+//        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);  //or Flags.REVEAL_LEFT
+//        configSplash.setRevealFlagY(Flags.REVEAL_TOP); //or Flags.REVEAL_TOP
+        configSplash.setAnimCircularRevealDuration(Flags.WITH_LOGO);
 
         //Customize Logo
         configSplash.setLogoSplash(R.drawable.splash);   //or any other drawable
-        configSplash.setAnimLogoSplashDuration(1100); //int ms
+        configSplash.setAnimLogoSplashDuration(1150); //int ms
         configSplash.setAnimLogoSplashTechnique(Techniques.ZoomIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
 
         configSplash.setTitleSplash(" ");

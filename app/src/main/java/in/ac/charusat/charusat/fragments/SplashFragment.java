60ffb12be6684b867e3a89a2c0284a5365e56bb1
Darshan <DozzerCoder>
2018-04-03 10:51:07 +0530

    Splash Screen is completely developed with animation and gradient effect is also given...!!! Reply if you like it...!!!!  :)

--- app/src/main/java/in/ac/charusat/charusat/fragments/SplashFragment.java
+++ app/src/main/java/in/ac/charusat/charusat/fragments/SplashFragment.java
@@ -2,13 +2,20 @@ package in.ac.charusat.charusat.fragments;
 
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.animation.SpringAnimation;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -27,18 +34,40 @@ public class SplashFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    CardView mCharusatLogo;
+    TextView mTitle;
+    TextView mFullFormTitle;
+    TextView mCaptionTitle;
+    TextView mSubtitle;
+    @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_splash,container,false);
+        mCharusatLogo = view.findViewById(R.id.card_view);
+        mTitle = view.findViewById(R.id.main_title);
+        mFullFormTitle = view.findViewById(R.id.fullForm_text);
+        mCaptionTitle = view.findViewById(R.id.fullInfo_text);
+        mSubtitle = view.findViewById(R.id.extraInfo_text);
+        Animation animation = AnimationUtils.loadAnimation(getActivity(),R.anim.custom_animation);
+        mCharusatLogo.startAnimation(animation);
+
+        Animation mTitleAnimation = AnimationUtils.loadAnimation(getActivity(),R.anim.text_animation);
+        Animation mFullFormAnimation = AnimationUtils.loadAnimation(getActivity(),R.anim.text_animation);
+        Animation mCaptionAnimation = AnimationUtils.loadAnimation(getActivity(),R.anim.text_animation);
+        Animation mSubtitleAnimation = AnimationUtils.loadAnimation(getActivity(),R.anim.text_animation);
+        mTitle.startAnimation(mTitleAnimation);
+        mFullFormTitle.setAnimation(mFullFormAnimation);
+        mCaptionTitle.setAnimation(mCaptionAnimation);
+        mSubtitle.setAnimation(mSubtitleAnimation);
+
+
         new Handler().postDelayed(() -> {
             /* Create an Intent that will start the Menu-Activity. */
             startActivity(new Intent(getActivity(),HomeActivity.class));
             Objects.requireNonNull(getActivity()).overridePendingTransition(R.anim.downtoup, R.anim.downtoup);
             getActivity().finish();
-
-        }, 2000);
+        }, 3500);
         return view;
 
     }

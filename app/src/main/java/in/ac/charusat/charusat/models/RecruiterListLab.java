80a99f8c302e7244bdfbb979aab65b47d6e3f77c
Darshan <DozzerCoder>
2018-04-12 14:50:08 +0530

    Layout changes and string of comapny are added

--- app/src/main/java/in/ac/charusat/charusat/models/RecruiterListLab.java
+++ app/src/main/java/in/ac/charusat/charusat/models/RecruiterListLab.java
@@ -21,20 +21,20 @@ public class RecruiterListLab {
 
     private void addITRecruiters() {
         mITRecruitersList = new ArrayList<>();
-        mITRecruitersList.add(new InfoList(R.drawable.tcs, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.acty_system, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.crestdata, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.e_infochip, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.gateway, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.indian_navy, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.indusa, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.infostrech, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.infotech, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.sibridge, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.synoverge, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.tatvasoft, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.volansys, R.string.about_us));
-        mITRecruitersList.add(new InfoList(R.drawable.vr_software, R.string.about_us));
+        mITRecruitersList.add(new InfoList(R.drawable.tcs, R.string.tcs_text));
+        mITRecruitersList.add(new InfoList(R.drawable.acty_system, R.string.acty_system_text));
+        mITRecruitersList.add(new InfoList(R.drawable.crestdata,R.string.crest_system_text));
+        mITRecruitersList.add(new InfoList(R.drawable.e_infochip, R.string.infochips_text));
+        mITRecruitersList.add(new InfoList(R.drawable.gateway, R.string.gateway_text));
+        mITRecruitersList.add(new InfoList(R.drawable.indian_navy, R.string.indian_navy_text));
+        mITRecruitersList.add(new InfoList(R.drawable.indusa, R.string.indusa_text));
+        mITRecruitersList.add(new InfoList(R.drawable.infostrech, R.string.inforstech_text));
+        mITRecruitersList.add(new InfoList(R.drawable.infotech, R.string.infotech_text));
+        mITRecruitersList.add(new InfoList(R.drawable.sibridge, R.string.sibridge_text));
+        mITRecruitersList.add(new InfoList(R.drawable.synoverge, R.string.synoverge_text));
+        mITRecruitersList.add(new InfoList(R.drawable.tatvasoft, R.string.tatvasoft_text));
+        mITRecruitersList.add(new InfoList(R.drawable.volansys, R.string.volanysis_text));
+        mITRecruitersList.add(new InfoList(R.drawable.vr_software, R.string.vr_text));
     }
 
     public ArrayList<InfoList> getITRecruitersList() {
